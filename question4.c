bea8b4255dd93f0024fb4d58aef6f93a259ae964
Alex Motyka <alex.motyka@uoit.net>
2018-01-29 15:09:07 -0500

    Source for question 4

---
+++ question4.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+//declare pointers
+char *professor;
+int *student_ids;
+int *grades;
+int i = 0;
+size_t length;
+size_t num;
+length = 256;
+
+
+
+//allocate memory
+professor = (char*) calloc (length,sizeof(char));
+
+//prompt professor 
+printf("Enter your name: ");
+scanf("%s", professor);
+
+printf("Enter the number of students to mark: ");
+scanf("%d", &i);
+
+num = i;
+
+student_ids = (int *) malloc(num);
+
+if (student_ids == NULL) {
+    /* Memory could not be allocated, the program should
+       handle the error here as appropriate. */
+    printf("Memory allocation for student ids failed!\n");
+} else {
+    printf("Success for student ids! Freeing mem now.\n");
+    free(student_ids);      
+    student_ids = NULL; 
+}
+
+grades = (int *) malloc(num);
+
+if (grades == NULL) {
+    /* Memory could not be allocated, the program should
+       handle the error here as appropriate. */
+    printf("Memory allocation for grades failed!\n");
+} else {
+    printf("Success for grades! Freeing mem now.\n");
+    free(grades);      
+    grades = NULL; 
+}
+}
+
+
+

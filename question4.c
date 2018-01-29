c168566bc3c2d59648e44a9cd260c7f14dbcd787
Alex Motyka <alex.motyka@uoit.net>
2018-01-29 15:19:45 -0500

    Added comments

--- question4.c
+++ question4.c
@@ -2,52 +2,57 @@
 #include <stdio.h>
 int main(void)
 {
-//declare pointers
-char *professor;
-int *student_ids;
-int *grades;
-int i = 0;
-size_t length;
-size_t num;
-length = 256;
+    //declare pointers
+    char *professor;
+    int *student_ids;
+    int *grades;
+    int i = 0;
+
+    //size for our pointers
+    size_t length;
+    size_t num;
+    length = 256;
 
 
 
-//allocate memory
-professor = (char*) calloc (length,sizeof(char));
+    //allocate memory
+    professor = (char*) calloc (length,sizeof(char));
 
-//prompt professor 
-printf("Enter your name: ");
-scanf("%s", professor);
+    //prompt professor 
+    printf("Enter your name: ");
+    scanf("%s", professor);
 
-printf("Enter the number of students to mark: ");
-scanf("%d", &i);
+    printf("Enter the number of students to mark: ");
+    scanf("%d", &i);
 
-num = i;
+    //store the number of students as our size for the following pointers
+    num = i;
 
-student_ids = (int *) malloc(num);
+    student_ids = (int *) malloc(num);
 
-if (student_ids == NULL) {
-    /* Memory could not be allocated, the program should
-       handle the error here as appropriate. */
-    printf("Memory allocation for student ids failed!\n");
-} else {
-    printf("Success for student ids! Freeing mem now.\n");
-    free(student_ids);      
-    student_ids = NULL; 
-}
+    if (student_ids == NULL) {
+        /* Memory could not be allocated, the program should
+        handle the error here as appropriate. */
+        printf("Memory allocation for student ids failed!\n");
+        exit(1);
+    } else {
+        printf("Success for student ids! Freeing mem now.\n");
+        free(student_ids);      
+        student_ids = NULL; 
+    }
 
-grades = (int *) malloc(num);
+    grades = (int *) malloc(num);
 
-if (grades == NULL) {
-    /* Memory could not be allocated, the program should
-       handle the error here as appropriate. */
-    printf("Memory allocation for grades failed!\n");
-} else {
-    printf("Success for grades! Freeing mem now.\n");
-    free(grades);      
-    grades = NULL; 
-}
+    if (grades == NULL) {
+        /* Memory could not be allocated, the program should
+        handle the error here as appropriate. */
+        printf("Memory allocation for grades failed!\n");
+        exit(1);
+    } else {
+        printf("Success for grades! Freeing mem now.\n");
+        free(grades);      
+        grades = NULL; 
+    }
 }
 
 

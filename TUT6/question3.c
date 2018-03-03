6e02d4130571580417f2e3a0f2ddd3d235619675
Alex Motyka <alex.motyka@uoit.net>
2018-03-03 14:12:10 -0500

    Started question 3

---
+++ TUT6/question3.c
@@ -0,0 +1,50 @@
+#define _XOPEN_SOURCE 700 // required for barriers to work
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
+int moving_sum[5] = {0,0,0,0,0};
+
+struct Data {
+    int num;
+    int index;
+};
+
+void *factorial(void *threadarg){
+    struct Data *my_data;
+    my_data = (struct Data *) threadarg;
+    int n = my_data->num;
+    int factorial=1;
+    int i;
+    for(i=1; i<=n; ++i){
+            factorial *= i;              
+        }
+        printf("Factorial of %d = %d\n", n, factorial);
+    //printf("%d\n", n);
+    //printf("Your data is %d, %d\n",my_data->index, my_data->num);
+    return 0;
+}
+int main(void)
+{
+    pthread_t tid[5];
+    
+    struct Data *my_data = malloc(5);
+    if(my_data==NULL)    
+        exit(1);
+    
+    for (int j = 0; j < 5; j++) {
+        printf("Enter a number: \n");
+        scanf("%d", &my_data[j].num);
+        my_data[j].index = j;
+        pthread_create(&tid[j], NULL, factorial, &my_data[j]);
+    }
+    for (int i = 0; i < 5; i++)
+        pthread_join(tid[i], NULL);
+    
+    //free(my_data);
+    my_data = NULL;
+    return 0;  
+}
+
